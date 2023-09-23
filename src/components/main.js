e4d54d91fa0137150ebd4e3e3704b7a609d96b26
EzatRashid <ezat.b.rashid@gmail.com>
2023-09-23 15:50:13 -0600

    added social media link

--- src/components/main.js
+++ src/components/main.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { AiOutlineInstagram } from 'react-icons/ai'
+
 
 const Main = () => {
   return (
@@ -8,12 +10,15 @@ const Main = () => {
             <p className='text-[50px]'>
                 <span className='bookborder'>BOOK </span>
                  AN APPOINTMENT WITH THE <span className='z-10'>CLIPGURU</span></p>
-            <bg className = 'bg-[#b38859] absolute h-20 w-[250px] right-[21.5%] top-[42%] -z-10'></bg>
-            <border className = 'bg-[#b38859] absolute h-20 w-[250px] right-[20.5%] top-[38%] bg-transparent border -z-20'></border>
+            <bg className = 'bg-[#b38859] absolute h-20 w-[250px] right-[20.5%] top-[37.5%] -z-10'></bg>
+            <border className = 'bg-[#b38859] absolute h-20 w-[250px] right-[19.5%] top-[35%] bg-transparent border -z-20'></border>
           
         </div>
         <div className="text-white ">
             <button className='bg-[#b38859] px-20 py-2 '>BOOK NOW</button>
+        </div>
+        <div className=" flex gap-2 mt-10 text-[#b38859] text-[30px]">
+            <AiOutlineInstagram/>
         </div>
     </div>
   )
